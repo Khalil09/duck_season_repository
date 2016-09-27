ce9bdd336140acf75fd1bdb74ba6054e56f9252d
khalil09 <khalilcarsten@gmail.com>
2016-09-27 14:38:39 -0300

    First exercice/First Commit

---
+++ URI/AD-HOC/1026.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, nv, ne, v1, v2, l, flag = 0;
+    list<int> grafo[30];
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d%d", &nv, &ne);
+
+        for (int j = 0; j < ne; j++) {
+            scanf("%d%d", &v1, &v2);
+
+
+        }
+    }
+    return 0;
+}
